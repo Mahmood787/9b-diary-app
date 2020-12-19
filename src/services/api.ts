3a0660d5c10a89c1f02f78be419af29502763715
Mahmood787 <ali_khan87@live.com>
2020-12-19 17:25:02 +0500

    url for netlify

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import { showAlert } from '../util';
 
 const http: AxiosInstance = axios.create({
-  baseURL: 'https://diaries-app.surge.sh/',
+  baseURL: 'https://9b-diary-app.netlify.app/',
 });
 
 http.defaults.headers.post['Content-Type'] = 'application/json';
